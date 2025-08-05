160bb83b4bee1f78207f671d79ccd0d3da7a6e45
k.wongso78 <k.wongso78@gmail.com>
2025-08-05 18:22:26 -0400

    Fix login and register route paths in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import GameWrapper from './pages/GameWrapper';
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/game" element={<GameWrapper />} />
             </Routes>
         </Router>
